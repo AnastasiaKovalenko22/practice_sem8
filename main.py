0c1dd796c973ea8857946470903ac8d0f8fd0ea5
AnastasiaKovalenko22 <81902015+AnastasiaKovalenko22@users.noreply.github.com>
2024-04-22 18:47:55 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from numpy import inf
 from scipy.integrate import solve_ivp
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,18 +35,20 @@ def Q1(a1):
 
 def error_is_small(y1, y2):
     for i in range(len(y1)):
-        v1 = y1[i]
-        v2 = y2[i]
-        err = np.linalg.norm(np.array(v2) - np.array(v1)) / np.linalg.norm(np.array(v1))
-        if err > 10**-5:
+        v1 = np.array(y1[i])
+        v2 = np.array(y2[i])
+        err = np.linalg.norm(v2 - v1)
+        if err == inf:
+            raise ValueError
+        if err/np.linalg.norm(v1) > 10**-5:
             return False
     return True
 
 
 t_eval = [0]
-for i in range(80):
-    t_eval.append(t_eval[i] + 0.25)
-t_span = [0, 20]
+for i in range(320):
+    t_eval.append(t_eval[i] + 0.5)
+t_span = [0, 160]
 
 
 def find_appropriate_k_and_count_moment(sigm):
@@ -59,15 +62,18 @@ def find_appropriate_k_and_count_moment(sigm):
         y_0 = get_initial_values(k)
         sol_2 = solve_ivp(a_n_set, t_span, y_0, 'RK45', t_eval, args=(k, sigm))
 
-        err_small = error_is_small(sol_1.y, sol_2.y)
-        k += 1
+        try:
+            err_small = error_is_small(sol_1.y, sol_2.y)
+            k += 1
+        except ValueError:
+            k += sigm
     print(f'sigma: ' + str(sigm) + ' k: ' + str(k-1))
     q1 = Q1(sol_2.y[1])
     plt.plot(t_eval, q1, label='sigma: ' + str(sigm) + ' k: ' + str(k-1))
 
 
 def main():
-    sigma_array = [0.05, 0.5, 1, 3, 5, 7, 10, 20]
+    sigma_array = [0.05, 0.5, 1, 3, 5, 7]
     for sigm in sigma_array:
         find_appropriate_k_and_count_moment(sigm)
     plt.title("Зависимость суммарного магнитного момента частиц от времени")
